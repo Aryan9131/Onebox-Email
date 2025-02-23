50f09c4fe325a3d3ba05fbebd98545aa0f9c420c
Aryan Nayak <aryannayak9131@gmail.com>
2025-02-23 13:52:31 +0530

    Added Core Functionalities

---
+++ server/features/slackNotifier.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const { WebClient } = require("@slack/web-api");
+
+const slack = new WebClient(process.env.SLACK_TOKEN);
+
+async function sendSlackNotification(email, category) {
+    try {
+        const message = `📩 *New Email Categorized as:* ${category}\n📝 *From:* ${email.from}\n💬 *Message:* ${email.body.substring(0, 100)}...`;
+        
+        await slack.chat.postMessage({
+            channel: process.env.SLACK_CHANNEL_ID,
+            text: message
+        });
+
+        console.log("Slack notification sent!");
+    } catch (err) {
+        console.error("Error sending Slack notification:", err);
+    }
+}
+
+module.exports = { sendSlackNotification };
---
+++ server/services/fetchAllEmails.js
@@ -0,0 +1,34 @@
+const { Client } = require("@elastic/elasticsearch");
+
+const esClient = new Client({ 
+  node: "http://localhost:9200",
+  auth: {
+    username: 'elastic',
+    password: 'Aryan@9131'
+  }
+});
+
+const indexName = "emails";
+
+
+async function fetchAllEmailsFromES() {
+    try {
+      let result=[];
+      const response = await esClient.search({
+        index: indexName,
+        size: 50, // Fetch up to 10 emails
+        query: { match_all: {} } // Fetch all emails
+      });
+  
+      if (response.hits.hits.length === 0) {
+        console.log(" No emails are present in Elasticsearch.");
+        return result;
+      } else {
+        return response.hits.hits;
+      }
+    } catch (error) {
+      console.error("Error while fetching emails:", error);
+    }
+  }
+  
+  module.exports= {fetchAllEmailsFromES};
